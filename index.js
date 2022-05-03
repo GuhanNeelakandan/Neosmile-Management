6244ebafc60d612bd4e63e08d0bee6effb5809be
Guhan <you@example.com>
2022-05-03 12:31:59 +0530

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors =require("cors");
 const mongoose=require("mongoose");
 const router = require("./routes/routes");
 
-
+const PORT = process.env.PORT || 8080
 
 const app=express();
 
@@ -13,8 +13,6 @@ app.use(express.json())
 app.use(cors())
 
 
-const port =8080;
-
 
 app.use('/',router)
 
@@ -23,8 +21,8 @@ app.use('/',router)
 
 const URI="mongodb+srv://Guhan:guhan@cluster0.gubnl.mongodb.net/crud?retryWrites=true&w=majority"
 mongoose.connect(URI).then(()=>{
-    app.listen(port,()=>{
-        console.log(`server is running on ${port}`)
+    app.listen(PORT,()=>{
+        console.log(`server is running on ${PORT}`)
     })
 }).catch((error)=>{
     console.log(error)
